1e1eeced2cbf80ac1a8229ee2f96757b0126523d
mbillig <marielle@billig.org>
2017-12-07 12:31:25 -0600

    minor tuning changes

--- classes/ea.py
+++ classes/ea.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from deap import creator, base, tools
 import random
 import os
@@ -10,13 +11,17 @@ IND_SIZE = 9
 INT_MIN = 1
 INT_MAX = 42
 POP_SIZE = 25
-GEN_NUM = 100
+GEN_NUM = 25
 P_CROSS = 0.25
-P_MUT = 0.25
+P_MUT = 0.4
 ELITE_NUM = 3
 
 def evaluateBot(individual):
-	runGameCommand = 'java main.RunGame 0 0 0 "java bot.BotStarter" "java frontLineBot.BotVars %s %s %s %s %s %s %s %s %s" 2>err.txt 1>out.txt' % (str(individual[0]), str(individual[1]), str(individual[2]), str(individual[3]), str(individual[4]), str(individual[5]), str(individual[6]), str(individual[7]), str(individual[8])) 
+	individual = list(map(abs, individual))
+	attack = int((individual[0] * 20)/42)
+	divisor = int((individual[1] * 10)/42)
+	leave = int((individual[2] * 8)/42)
+	runGameCommand = 'java main.RunGame 0 0 0 "java frontLineBot.BotStarter" "java frontLineBot.BotVars %s %s %s %s %s %s %s %s %s" 2>err.txt 1>out.txt' % (str(attack), str(divisor), str(leave), str(individual[3]), str(individual[4]), str(individual[5]), str(individual[6]), str(individual[7]), str(individual[8])) 
 #	print(os.getcwd())
 #	print(runGameCommand)
 	os.system(runGameCommand)
@@ -31,7 +36,9 @@ def evaluateBot(individual):
 #	print(fitness)
 	return fitness
 
-
+def selElitistAndRoulette(individuals, k, num_elitist):
+    return tools.selBest(individuals, int(num_elitist)) + tools.selRoulette(individuals, int(k-num_elitist), fit_attr='fitness')
+    
 creator.create("BotFitness", base.Fitness, weights=(-1.0,))
 creator.create("Individual", list, fitness=creator.BotFitness)
 
@@ -42,9 +49,13 @@ toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.att
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 pop = toolbox.population(n=POP_SIZE)
 
+
 toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.2)
-toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("mutate", tools.mutGaussian, mu=10, sigma=7, indpb=0.2)
+toolbox.register("selBestElite", tools.selBest, k=ELITE_NUM)
+toolbox.register("selBestSingle", tools.selBest, k=1)
+toolbox.register("selRouletteElite", tools.selRoulette)
+toolbox.register("select", selElitistAndRoulette, num_elitist=ELITE_NUM)
 toolbox.register("evaluate", evaluateBot)
 
 stats = tools.Statistics(key=lambda ind: ind.fitness.values)
@@ -52,6 +63,12 @@ stats.register("avg", numpy.mean)
 stats.register("std", numpy.std)
 stats.register("min", numpy.min)
 stats.register("max", numpy.max)
+
+print("calculating initial population fitness")
+invalid_ind = [ind for ind in pop if not ind.fitness.valid]
+fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+for ind, fit in zip(invalid_ind, fitnesses):
+	ind.fitness.values = fit
 
 ##ind1 = toolbox.individual()
 ##ind1.fitness.values = evaluate(ind1)
@@ -70,8 +87,11 @@ stats.register("max", numpy.max)
 for gen in range(GEN_NUM):
 	start = datetime.datetime.now()
 	print("generation: " + str(gen))
-	offspring = toolbox.select(pop, len(pop))
+	offspring = toolbox.selRouletteElite(pop, len(pop))
 	offspring = list(map(toolbox.clone, offspring))
+	
+	best = toolbox.selBestElite(pop)
+	best = list(map(toolbox.clone, best))
 
 	for child1, child2 in zip(offspring[::2], offspring[1::2]):
 		if random.random() < P_CROSS:
@@ -84,11 +104,14 @@ for gen in range(GEN_NUM):
 			toolbox.mutate(mutant)
 			del mutant.fitness.values
 
+	offspring = best + offspring
 	invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
 	fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
 	for ind, fit in zip(invalid_ind, fitnesses):
 		ind.fitness.values = fit
 
+
+	bestInd = toolbox.selBestSingle(offspring)
 	pop[:] = offspring
 	record = stats.compile(pop)
 	print(record)
@@ -103,10 +126,10 @@ for gen in range(GEN_NUM):
 	delta = end-start
 
 	print(delta)
-	csvRow = "%s, %s, %s, %s, %s, %s\n" % (str(gen), str(min(fits)), str(max(fits)), str(mean), str(std), str(delta))
-	with open("GenStats2.csv", "a") as file:
+	csvRow = "%s, %s, %s, %s, %s, %s, %s\n" % (str(gen), str(min(fits)), str(max(fits)), str(mean), str(std), str(delta), str(bestInd))
+	with open("GenStats6.csv", "a") as file:
 		file.write(csvRow)
-	with open("GenReports.txt", "a") as file:
+	with open("GenReports6.txt", "a") as file:
 		file.write(str(record) + "\n")
 
 
